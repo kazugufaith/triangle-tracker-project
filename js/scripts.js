6504e528206d27e09e5b70ac1eebc18eda66c7f5
faithkazungu <faithmugesia@gmail.com>
2019-03-23 22:47:41 +0300

    added Licence file

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function tracker(){
+  //Initialize 3 sides
+    var side1 = document.getElementById("sideA").value;
+    var side2 = document.getElementById("sideB").value;
+    var side3 = document.getElementById("sideC").value;
+  //Change input to integer value
+    var sideA = parseInt(side1);
+    var sideB = parseInt(side2);
+    var sideC = parseInt(side3);
